d426158850a0690e6c9e50856868e1bda07396cd
shambhabya <shambhabyamedhi@192.168.1.2>
2023-11-18 02:07:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// app.js
+const express = require('express');
+const faker = require('faker');
+const specialities = require('./specialities');
+const BASE_URL = process.env.BASE_URL
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+const generateFakeData = (speciality) => {
+    return {
+      name: faker.name.findName(),
+      experience: faker.datatype.number({ min: 1, max: 30 }),
+      fee: faker.datatype.number({ min: 50, max: 500 }),
+      clinicLocation: faker.address.city()
+    };
+  };
+
+
+app.get('/api/specialities', (req, res) => {
+  res.json(specialities);
+});
+
+app.get('/api/doctors/:speciality', (req, res) => {
+    const specialityName = req.params.speciality;
+    const numberOfDoctors = 7; 
+  
+    const doctors = Array.from({ length: numberOfDoctors }, () => generateFakeData(specialityName));
+  
+    res.json(doctors);
+  });
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ specialities.js
@@ -0,0 +1,51 @@
+// specialities.js
+const specialities = [
+    {
+      name: 'Cardiologist',
+      image: 'cardiologist.jpg',
+      description: 'Cardiologists are medical doctors who specialize in the diagnosis and treatment of heart-related conditions.',
+    },
+    {
+      name: 'Dermatologist',
+      image: 'dermatologist.jpg',
+      description: 'Dermatologists focus on the health of the skin, hair, and nails, diagnosing and treating various skin conditions.',
+    },
+    {
+      name: 'Pediatrician',
+      image: 'pediatrician.jpg',
+      description: 'Pediatricians are doctors who specialize in the care of infants, children, and adolescents.',
+    },
+    {
+      name: 'Orthopedic',
+      image: 'orthopedic.jpg',
+      description: 'Orthopedic surgeons specialize in the diagnosis and treatment of disorders of the musculoskeletal system.',
+    },
+    {
+      name: 'Ophthalmologist',
+      image: 'ophthalmologist.jpg',
+      description: 'Ophthalmologists are medical doctors who specialize in the diagnosis and treatment of eye disorders.',
+    },
+    {
+      name: 'Gastroenterologist',
+      image: 'gastroenterologist.jpg',
+      description: 'Gastroenterologists focus on the diagnosis and treatment of disorders of the digestive system.',
+    },
+    {
+      name: 'Neurologist',
+      image: 'neurologist.jpg',
+      description: 'Neurologists specialize in the diagnosis and treatment of disorders of the nervous system.',
+    },
+    {
+      name: 'Endocrinologist',
+      image: 'endocrinologist.jpg',
+      description: 'Endocrinologists focus on the diagnosis and treatment of disorders related to the endocrine system and hormones.',
+    },
+    {
+      name: 'Urologist',
+      image: 'urologist.jpg',
+      description: 'Urologists specialize in the diagnosis and treatment of disorders of the urinary tract and male reproductive system.',
+    }
+  ];
+  
+  module.exports = specialities;
+  
