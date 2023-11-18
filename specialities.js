611d4f1ffff3fd5288019aff324144ff5bd0ed95
shambhabya <shambhabyamedhi@192.168.1.2>
2023-11-18 13:34:54 +0530

    little modification

--- specialities.js
+++ specialities.js
@@ -2,47 +2,47 @@
 const specialities = [
     {
       name: 'Cardiologist',
-      image: 'cardiologist.jpg',
+      image: 'Cardiologist.jpg',
       description: 'Cardiologists are medical doctors who specialize in the diagnosis and treatment of heart-related conditions.',
     },
     {
       name: 'Dermatologist',
-      image: 'dermatologist.jpg',
+      image: 'Dermatologist.jpg',
       description: 'Dermatologists focus on the health of the skin, hair, and nails, diagnosing and treating various skin conditions.',
     },
     {
       name: 'Pediatrician',
-      image: 'pediatrician.jpg',
+      image: 'Pediatrician.jpg',
       description: 'Pediatricians are doctors who specialize in the care of infants, children, and adolescents.',
     },
     {
       name: 'Orthopedic',
-      image: 'orthopedic.jpg',
+      image: 'Orthopedic.jpg',
       description: 'Orthopedic surgeons specialize in the diagnosis and treatment of disorders of the musculoskeletal system.',
     },
     {
       name: 'Ophthalmologist',
-      image: 'ophthalmologist.jpg',
+      image: 'Ophthalmologist.jpg',
       description: 'Ophthalmologists are medical doctors who specialize in the diagnosis and treatment of eye disorders.',
     },
     {
       name: 'Gastroenterologist',
-      image: 'gastroenterologist.jpg',
+      image: 'Gastroenterologist.jpg',
       description: 'Gastroenterologists focus on the diagnosis and treatment of disorders of the digestive system.',
     },
     {
       name: 'Neurologist',
-      image: 'neurologist.jpg',
+      image: 'Neurologist.jpg',
       description: 'Neurologists specialize in the diagnosis and treatment of disorders of the nervous system.',
     },
     {
       name: 'Endocrinologist',
-      image: 'endocrinologist.jpg',
+      image: 'Endocrinologist.jpg',
       description: 'Endocrinologists focus on the diagnosis and treatment of disorders related to the endocrine system and hormones.',
     },
     {
       name: 'Urologist',
-      image: 'urologist.jpg',
+      image: 'Urologist.jpg',
       description: 'Urologists specialize in the diagnosis and treatment of disorders of the urinary tract and male reproductive system.',
     }
   ];
